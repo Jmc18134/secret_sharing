376357a3799046faa2080ff648546fd5e61bc0b6
james <tmb18134@uni.strath.ac.uk>
2020-09-03 19:41:12 +0100

    Started work on encode screen

--- secret-sharing/encode.py
+++ secret-sharing/encode.py
@@ -20,3 +20,25 @@ def encode_secret():
             flash(error)
         return render_template('encode_secret.html')
     return render_template('encode_secret.html')
+
+@bp.route('/results', methods=("POST",))
+def show_results():
+    n = request.form['shares']
+    k = request.form['keys_required']
+    secret = request.form['secret']
+
+    error = None
+    if not (n and k and secret):
+        error = 'All fields must be filled in!'
+
+    if error is not None:
+        flash(error)
+
+    '''
+    # TODO: Fix this, sort out how the polynomials are actually represented
+    rand_poly = Polynomial.secret(n, k, secret)
+    shares = [rand_poly.mod_eval(i) for i in range(1, n+1)]
+    
+    return render_template('results.html', shares=shares)
+    '''
+    return b"Hello, world!"
