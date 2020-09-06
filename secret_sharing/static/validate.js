48730498386e66411147b5a96952ef6796a67ea8
james <tmb18134@uni.strath.ac.uk>
2020-09-06 23:24:37 +0100

    Added basic js

--- secret_sharing/static/validate.js
+++ secret_sharing/static/validate.js
@@ -1,10 +1,24 @@
-function validateEncoding() {
-  var x = parseInt(document.getElementById("shares").value);
-  var y = parseInt(document.getElementById("keys_required").value);
-  
-  
-  if (x < y) {
-    alert("N must be greater or equal to K.");
-    return false;
+document.getElementById("encode").addEventListener("submit", validate);
+
+function validate(ev) {
+  var shares = parseFloat(document.getElementById("shares").value);
+  var keys = parseFloat(document.getElementById("keys_required").value);
+  var valid = true;
+  var err_msg = document.getElementById("input_error");
+
+  if (shares != NaN && keys != NaN) {
+    if (shares < keys) {
+      valid = false;
+      err_msg.innerHTML = "N must be less than K.";
+    }
+  } else {
+    valid = false;
+    err_msg.innerHTML = "N and K must be numbers.";
+  }
+
+  if (!valid) {
+    ev.preventDefault();
+  } else {
+    err_msg = "";
   }
 }
