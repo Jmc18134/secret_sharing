8ae3e5c1a1ec8555f0d4ff6af3705a161baada57
james <tmb18134@uni.strath.ac.uk>
2020-09-10 18:42:38 +0100

    Added err handling

--- secret_sharing/static/validate.js
+++ secret_sharing/static/validate.js
@@ -1,10 +1,20 @@
 document.getElementById("encode").addEventListener("submit", validate);
 
 function validate(ev) {
-  var shares = parseFloat(document.getElementById("shares").value);
-  var keys = parseFloat(document.getElementById("keys_required").value);
+  var shares = document.getElementById("shares").value;
+  var keys = document.getElementById("keys_required").value;
+  var secret = document.getElementById("secret").value;
   var valid = true;
   var err_msg = document.getElementById("input_error");
+
+  if (shares == "" || keys  == "" || secret == "") {
+      err_msg.innerHTML = "All fields must be filled in.";
+      valid = false;
+  }
+
+  shares = parseInt(shares)
+  keys = parseInt(keys)
+  secret = parseInt(secret)
 
   if (shares != NaN && keys != NaN) {
     if (shares < keys) {
