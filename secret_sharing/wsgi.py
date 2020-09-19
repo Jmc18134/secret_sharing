43bdd6e9d31e07b0d164b2476e28b7dd72e9af7d
james <tmb18134@uni.strath.ac.uk>
2020-09-19 22:06:22 +0100

    Typo

--- secret_sharing/wsgi.py
+++ secret_sharing/wsgi.py
@@ -1,6 +1,6 @@
-from whitenoise import WhiteNoise
+#from whitenoise import WhiteNoise
 
 from secret_sharing import create_app
 
 app = create_app()
-app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
+#app.wsgi_app = WhiteNoise(app.wsgi_app, root='static/')
